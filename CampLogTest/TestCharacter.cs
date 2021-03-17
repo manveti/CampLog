2f9ad277c8fa7a3f2ec1f248fa73e57d1f962c49
manveti <manveti@users.noreply.github.com>
2021-03-16 21:24:54 -0700

    Add tests for Character methods.

--- CampLogTest/TestCharacter.cs
+++ CampLogTest/TestCharacter.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 using System.Runtime.Serialization;
 
 using CampLog;
@@ -136,6 +137,117 @@ namespace CampLogTest {
             Assert.AreEqual(sp.value.Count, 2);
             Assert.IsTrue(sp.value.Contains("bloh"));
             Assert.IsTrue(sp.value.Contains("bleh"));
+        }
+
+        private Character get_test_character() {
+            Character c = new Character("Krakrox the Barbarian");
+
+            c.properties.value["Player"] = new CharTextProperty("Philip");
+            c.properties.value["XP"] = new CharNumProperty(42);
+            CharSetProperty feats = new CharSetProperty();
+            feats.value.Add("Power Attack");
+            feats.value.Add("Cleave");
+            c.properties.value["Feats"] = feats;
+            CharDictProperty skills = new CharDictProperty();
+            skills.value["Diplomacy"] = new CharNumProperty(0);
+            skills.value["Seal Clubbing"] = new CharNumProperty(9001);
+            c.properties.value["Skills"] = skills;
+
+            return c;
+        }
+
+        [TestMethod]
+        public void test_get_property() {
+            Character c = this.get_test_character();
+
+            CharTextProperty prop = c.get_property(new List<string>() { "Player" }) as CharTextProperty;
+            Assert.IsFalse(prop is null);
+            Assert.AreEqual(prop.value, "Philip");
+        }
+
+        [TestMethod]
+        public void test_get_property_nested() {
+            Character c = this.get_test_character();
+
+            CharNumProperty prop = c.get_property(new List<string>() { "Skills", "Seal Clubbing" }) as CharNumProperty;
+            Assert.IsFalse(prop is null);
+            Assert.AreEqual(prop.value, 9001);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void test_get_property_no_such_property() {
+            Character c = this.get_test_character();
+
+            c.get_property(new List<string>() { "Skills", "Fishing" });
+        }
+
+        [TestMethod]
+        public void test_set_property_new() {
+            Character c = this.get_test_character();
+            CharNumProperty new_prop = new CharNumProperty(42);
+
+            CharProperty old_prop = c.set_property(new List<string>() { "Skills", "Fury" }, new_prop);
+            Assert.IsTrue(old_prop is null);
+
+            CharNumProperty prop = c.get_property(new List<string>() { "Skills", "Fury" }) as CharNumProperty;
+            Assert.IsFalse(prop is null);
+            Assert.AreEqual(prop.value, new_prop.value);
+        }
+
+        [TestMethod]
+        public void test_set_property_replace() {
+            Character c = this.get_test_character();
+            CharTextProperty new_prop = new CharTextProperty("Krakrox has no time for skills!");
+
+            CharDictProperty old_prop = c.set_property(new List<string>() { "Skills" }, new_prop) as CharDictProperty;
+            Assert.IsFalse(old_prop is null);
+            Assert.AreEqual(old_prop.value.Count, 2);
+            Assert.IsTrue(old_prop.value.ContainsKey("Diplomacy"));
+            Assert.IsTrue(old_prop.value.ContainsKey("Seal Clubbing"));
+
+            CharTextProperty prop = c.get_property(new List<string>() { "Skills" }) as CharTextProperty;
+            Assert.IsFalse(prop is null);
+            Assert.AreEqual(prop.value, new_prop.value);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void test_set_property_no_such_parent() {
+            Character c = this.get_test_character();
+            CharNumProperty new_prop = new CharNumProperty(2);
+
+            c.set_property(new List<string>() { "Spells per Day", "Level 1" }, new_prop);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void test_set_property_non_dict_parent() {
+            Character c = this.get_test_character();
+            CharNumProperty new_prop = new CharNumProperty(1000);
+
+            c.set_property(new List<string>() { "XP", "Next Level" }, new_prop);
+        }
+
+        [TestMethod]
+        public void test_remove_property() {
+            Character c = this.get_test_character();
+
+            Assert.IsTrue(c.properties.value.ContainsKey("XP"));
+
+            CharNumProperty old_prop = c.remove_property(new List<string>() { "XP" }) as CharNumProperty;
+            Assert.IsFalse(old_prop is null);
+            Assert.AreEqual(old_prop.value, 42);
+
+            Assert.IsFalse(c.properties.value.ContainsKey("XP"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void test_remove_property_no_such_property() {
+            Character c = this.get_test_character();
+
+            c.remove_property(new List<string>() { "Spells" });
         }
     }
 }
