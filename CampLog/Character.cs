c3936448361ed3f2c0a0eceb32d7b129b695f864
manveti <manveti@users.noreply.github.com>
2021-03-16 20:57:53 -0700

    Add initial work on characters.

---
+++ CampLog/Character.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace CampLog {
+    [KnownType(typeof(CharTextProperty))]
+    [KnownType(typeof(CharNumProperty))]
+    [KnownType(typeof(CharSetProperty))]
+    [KnownType(typeof(CharDictProperty))]
+    [Serializable]
+    public abstract class CharProperty { }
+
+
+    [Serializable]
+    public class CharTextProperty : CharProperty {
+        public string value;
+
+        public CharTextProperty(string value) {
+            this.value = value;
+        }
+    }
+
+
+    [Serializable]
+    public class CharNumProperty : CharProperty {
+        public decimal value;
+
+        public CharNumProperty(decimal value) {
+            this.value = value;
+        }
+    }
+
+
+    [Serializable]
+    public class CharSetProperty : CharProperty {
+        public HashSet<string> value;
+
+        public CharSetProperty() {
+            this.value = new HashSet<string>();
+        }
+    }
+
+
+    [Serializable]
+    public class CharDictProperty : CharProperty {
+        public Dictionary<string, CharProperty> value;
+
+        public CharDictProperty() {
+            this.value = new Dictionary<string, CharProperty>();
+        }
+    }
+
+
+    [Serializable]
+    public class Character {
+        public string name;
+        public CharDictProperty properties;
+
+        public Character(string name) {
+            this.name = name;
+            this.properties = new CharDictProperty();
+        }
+
+        public CharProperty get_property(List<string> path) {
+            CharProperty prop = this.properties;
+
+            foreach (string s in path) {
+                if (prop is not CharDictProperty dict_prop) { throw new ArgumentOutOfRangeException(nameof(path)); }
+                if (!dict_prop.value.ContainsKey(s)) { throw new ArgumentOutOfRangeException(nameof(path)); }
+                prop = dict_prop.value[s];
+            }
+            return prop;
+        }
+
+        public CharProperty set_property(List<string> path, CharProperty prop) {
+            CharProperty result = null;
+            CharDictProperty dict_prop = this.properties;
+            int i;
+
+            if (prop is null) { throw new ArgumentNullException(nameof(prop)); }
+
+            for (i = 0; i < path.Count - 1; i++) {
+                if (!dict_prop.value.ContainsKey(path[i])) { throw new ArgumentOutOfRangeException(nameof(path)); }
+                dict_prop = dict_prop.value[path[i]] as CharDictProperty;
+                if (dict_prop is null) { throw new ArgumentOutOfRangeException(nameof(path)); }
+            }
+
+            if (dict_prop.value.ContainsKey(path[i])) {
+                result = dict_prop.value[path[i]];
+            }
+            dict_prop.value[path[i]] = prop;
+
+            return result;
+        }
+
+        public CharProperty remove_property(List<string> path) {
+            CharDictProperty dict_prop = this.properties;
+            int i;
+
+            for (i = 0; i < path.Count - 1; i++) {
+                if (!dict_prop.value.ContainsKey(path[i])) { throw new ArgumentOutOfRangeException(nameof(path)); }
+                dict_prop = dict_prop.value[path[i]] as CharDictProperty;
+                if (dict_prop is null) { throw new ArgumentOutOfRangeException(nameof(path)); }
+            }
+            if (!dict_prop.value.ContainsKey(path[i])) { throw new ArgumentOutOfRangeException(nameof(path)); }
+
+            CharProperty result = dict_prop.value[path[i]];
+            dict_prop.value.Remove(path[i]);
+
+            return result;
+        }
+    }
+}
---
+++ CampLogTest/TestCharacter.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Runtime.Serialization;
+
+using CampLog;
+
+namespace CampLogTest {
+    [TestClass]
+    public class TestCharTextProperty {
+        [TestMethod]
+        public void test_serialization() {
+            CharTextProperty foo = new CharTextProperty("blah"), bar;
+            DataContractSerializer fmt = new DataContractSerializer(typeof(CharTextProperty));
+            using (System.IO.MemoryStream ms = new System.IO.MemoryStream()) {
+                fmt.WriteObject(ms, foo);
+                ms.Seek(0, System.IO.SeekOrigin.Begin);
+                System.Xml.XmlDictionaryReader xr = System.Xml.XmlDictionaryReader.CreateTextReader(ms, new System.Xml.XmlDictionaryReaderQuotas());
+                bar = (CharTextProperty)(fmt.ReadObject(xr, true));
+            }
+            Assert.AreEqual(foo.value, bar.value);
+        }
+    }
+
+
+    [TestClass]
+    public class TestCharNumProperty {
+        [TestMethod]
+        public void test_serialization() {
+            CharNumProperty foo = new CharNumProperty(1.3m), bar;
+            DataContractSerializer fmt = new DataContractSerializer(typeof(CharNumProperty));
+            using (System.IO.MemoryStream ms = new System.IO.MemoryStream()) {
+                fmt.WriteObject(ms, foo);
+                ms.Seek(0, System.IO.SeekOrigin.Begin);
+                System.Xml.XmlDictionaryReader xr = System.Xml.XmlDictionaryReader.CreateTextReader(ms, new System.Xml.XmlDictionaryReaderQuotas());
+                bar = (CharNumProperty)(fmt.ReadObject(xr, true));
+            }
+            Assert.AreEqual(foo.value, bar.value);
+        }
+    }
+
+
+    [TestClass]
+    public class TestCharSetProperty {
+        [TestMethod]
+        public void test_serialization() {
+            CharSetProperty foo = new CharSetProperty(), bar;
+            foo.value.Add("blah");
+            foo.value.Add("bloh");
+            DataContractSerializer fmt = new DataContractSerializer(typeof(CharSetProperty));
+            using (System.IO.MemoryStream ms = new System.IO.MemoryStream()) {
+                fmt.WriteObject(ms, foo);
+                ms.Seek(0, System.IO.SeekOrigin.Begin);
+                System.Xml.XmlDictionaryReader xr = System.Xml.XmlDictionaryReader.CreateTextReader(ms, new System.Xml.XmlDictionaryReaderQuotas());
+                bar = (CharSetProperty)(fmt.ReadObject(xr, true));
+            }
+            Assert.AreEqual(foo.value.Count, bar.value.Count);
+            foreach (string s in foo.value) {
+                Assert.IsTrue(bar.value.Contains(s));
+            }
+        }
+    }
+
+
+    [TestClass]
+    public class TestCharDictProperty {
+        [TestMethod]
+        public void test_serialization() {
+            CharDictProperty foo = new CharDictProperty(), bar;
+            CharTextProperty tprop = new CharTextProperty("blah");
+            CharNumProperty nprop = new CharNumProperty(1.23m);
+            CharSetProperty sprop = new CharSetProperty();
+            sprop.value.Add("bloh");
+            sprop.value.Add("bleh");
+            foo.value["Some Text"] = tprop;
+            foo.value["Some Number"] = nprop;
+            foo.value["Some Collection"] = sprop;
+            DataContractSerializer fmt = new DataContractSerializer(typeof(CharDictProperty));
+            using (System.IO.MemoryStream ms = new System.IO.MemoryStream()) {
+                fmt.WriteObject(ms, foo);
+                ms.Seek(0, System.IO.SeekOrigin.Begin);
+                System.Xml.XmlDictionaryReader xr = System.Xml.XmlDictionaryReader.CreateTextReader(ms, new System.Xml.XmlDictionaryReaderQuotas());
+                bar = (CharDictProperty)(fmt.ReadObject(xr, true));
+            }
+            Assert.AreEqual(foo.value.Count, bar.value.Count);
+            foreach (string s in foo.value.Keys) {
+                Assert.IsTrue(bar.value.ContainsKey(s));
+            }
+            CharTextProperty tp = bar.value["Some Text"] as CharTextProperty;
+            Assert.IsFalse(tp is null);
+            Assert.AreEqual(tp.value, "blah");
+            CharNumProperty np = bar.value["Some Number"] as CharNumProperty;
+            Assert.IsFalse(np is null);
+            Assert.AreEqual(np.value, 1.23m);
+            CharSetProperty sp = bar.value["Some Collection"] as CharSetProperty;
+            Assert.IsFalse(sp is null);
+            Assert.AreEqual(sp.value.Count, 2);
+            Assert.IsTrue(sp.value.Contains("bloh"));
+            Assert.IsTrue(sp.value.Contains("bleh"));
+        }
+    }
+
+
+    [TestClass]
+    public class TestCharacter {
+        [TestMethod]
+        public void test_serialization() {
+            Character foo = new Character("Somebody"), bar;
+            CharTextProperty tprop = new CharTextProperty("blah");
+            CharNumProperty nprop = new CharNumProperty(1.23m);
+            CharSetProperty sprop = new CharSetProperty();
+            sprop.value.Add("bloh");
+            sprop.value.Add("bleh");
+            foo.properties.value["Some Text"] = tprop;
+            foo.properties.value["Some Number"] = nprop;
+            foo.properties.value["Some Collection"] = sprop;
+            DataContractSerializer fmt = new DataContractSerializer(typeof(Character));
+            using (System.IO.MemoryStream ms = new System.IO.MemoryStream()) {
+                fmt.WriteObject(ms, foo);
+                ms.Seek(0, System.IO.SeekOrigin.Begin);
+                System.Xml.XmlDictionaryReader xr = System.Xml.XmlDictionaryReader.CreateTextReader(ms, new System.Xml.XmlDictionaryReaderQuotas());
+                bar = (Character)(fmt.ReadObject(xr, true));
+            }
+            Assert.AreEqual(foo.name, bar.name);
+            Assert.AreEqual(foo.properties.value.Count, bar.properties.value.Count);
+            foreach (string s in foo.properties.value.Keys) {
+                Assert.IsTrue(bar.properties.value.ContainsKey(s));
+            }
+            CharTextProperty tp = bar.properties.value["Some Text"] as CharTextProperty;
+            Assert.IsFalse(tp is null);
+            Assert.AreEqual(tp.value, "blah");
+            CharNumProperty np = bar.properties.value["Some Number"] as CharNumProperty;
+            Assert.IsFalse(np is null);
+            Assert.AreEqual(np.value, 1.23m);
+            CharSetProperty sp = bar.properties.value["Some Collection"] as CharSetProperty;
+            Assert.IsFalse(sp is null);
+            Assert.AreEqual(sp.value.Count, 2);
+            Assert.IsTrue(sp.value.Contains("bloh"));
+            Assert.IsTrue(sp.value.Contains("bleh"));
+        }
+    }
+}
