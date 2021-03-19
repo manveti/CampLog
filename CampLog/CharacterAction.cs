ed31fead2d4b961359fac792eb2710e9fefcb213
manveti <manveti@users.noreply.github.com>
2021-03-19 01:34:12 -0700

    Add character property (set and adjust) actions.

--- CampLog/CharacterAction.cs
+++ CampLog/CharacterAction.cs
@@ -4,10 +4,10 @@ using System.Collections.Generic;
 namespace CampLog {
     [Serializable]
     public class ActionCharacterSet : EventAction {
-        public Guid guid;
-        public Character from;
-        public Character to;
-        public bool restore;
+        public readonly Guid guid;
+        public readonly Character from;
+        public readonly Character to;
+        public readonly bool restore;
 
         public override string description {
             get {
@@ -29,6 +29,8 @@ namespace CampLog {
 
         public ActionCharacterSet(Guid guid, Character from, Character to, bool restore = false) {
             if ((from is null) && (to is null)) { throw new ArgumentNullException(nameof(to)); }
+            if (from is not null) { from = from.copy(); }
+            if (to is not null) { to = to.copy(); }
             this.guid = guid;
             this.from = from;
             this.to = to;
@@ -76,5 +78,98 @@ namespace CampLog {
     }
 
 
-    //TODO: ...
+    [Serializable]
+    public class ActionCharacterPropertySet : EventAction {
+        public readonly Guid guid;
+        public readonly List<string> path;
+        public readonly CharProperty from;
+        public readonly CharProperty to;
+
+        public override string description {
+            get {
+                string path_str = String.Join(":", this.path);
+                if (this.from is null) {
+                    return string.Format("Set character property {0}", path_str);
+                }
+                if (this.to is null) {
+                    return string.Format("Remove character property {0}", path_str);
+                }
+                return string.Format("Update character property {0}", path_str);
+            }
+        }
+
+        public ActionCharacterPropertySet(Guid guid, List<string> path, CharProperty from, CharProperty to) {
+            if (path is null) { throw new ArgumentNullException(nameof(path)); }
+            if (path.Count <= 0) { throw new ArgumentOutOfRangeException(nameof(path)); }
+            if ((from is null) && (to is null)) { throw new ArgumentNullException(nameof(to)); }
+            if (from is not null) { from = from.copy(); }
+            if (to is not null) { to = to.copy(); }
+            this.guid = guid;
+            this.path = new List<string>(path);
+            this.from = from;
+            this.to = to;
+        }
+
+        public override void apply(CampaignState state) {
+            if (this.to is null) {
+                // remove existing property
+                state.characters.characters[this.guid].remove_property(path);
+            }
+            else {
+                // add new property or update existing property
+                state.characters.characters[this.guid].set_property(path, this.to.copy());
+            }
+        }
+
+        public override void revert(CampaignState state) {
+            if (this.from is null) {
+                // revert addition of new property
+                state.characters.characters[this.guid].remove_property(path);
+            }
+            else {
+                // revert removal or update of existing property
+                state.characters.characters[this.guid].set_property(path, this.from.copy());
+            }
+        }
+    }
+
+
+    [Serializable]
+    public class ActionCharacterPropertyAdjust : EventAction {
+        public readonly Guid guid;
+        public readonly List<string> path;
+        public readonly CharProperty subtract;
+        public readonly CharProperty add;
+
+        public override string description {
+            get {
+                string path_str = String.Join(":", this.path);
+                return string.Format("Adjust character property {0}", path_str);
+            }
+        }
+
+        public ActionCharacterPropertyAdjust(Guid guid, List<string> path, CharProperty subtract, CharProperty add) {
+            if (path is null) { throw new ArgumentNullException(nameof(path)); }
+            if (path.Count <= 0) { throw new ArgumentOutOfRangeException(nameof(path)); }
+            if ((subtract is null) && (add is null)) { throw new ArgumentNullException(nameof(add)); }
+            if (subtract is not null) { subtract = subtract.copy(); }
+            if (add is not null) { add = add.copy(); }
+            this.guid = guid;
+            this.path = path;
+            this.subtract = subtract;
+            this.add = add;
+        }
+
+        public override void apply(CampaignState state) {
+            CharProperty prop = state.characters.characters[this.guid].get_property(this.path);
+            if (this.subtract is not null) { prop.subtract(this.subtract); }
+            if (this.add is not null) { prop.add(this.add); }
+        }
+
+        public override void revert(CampaignState state) {
+            CharProperty prop = state.characters.characters[this.guid].get_property(this.path);
+            if (this.add is not null) { prop.subtract(this.add); }
+            if (this.subtract is not null) { prop.add(this.subtract); }
+        }
+    }
 }
--- CampLogTest/TestCharacterAction.cs
+++ CampLogTest/TestCharacterAction.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 using System.Runtime.Serialization;
 
 using CampLog;
@@ -239,5 +240,326 @@ namespace CampLogTest {
     }
 
 
-    //TODO: ...
+    [TestClass]
+    public class TestActionCharacterPropertySet {
+        [TestMethod]
+        public void test_serialization() {
+            CharNumProperty jump = new CharNumProperty(42);
+            ActionCharacterPropertySet foo = new ActionCharacterPropertySet(Guid.NewGuid(), new List<string>() { "Skills", "Jump" }, null, jump), bar;
+            DataContractSerializer fmt = new DataContractSerializer(typeof(ActionCharacterPropertySet));
+            using (System.IO.MemoryStream ms = new System.IO.MemoryStream()) {
+                fmt.WriteObject(ms, foo);
+                ms.Seek(0, System.IO.SeekOrigin.Begin);
+                System.Xml.XmlDictionaryReader xr = System.Xml.XmlDictionaryReader.CreateTextReader(ms, new System.Xml.XmlDictionaryReaderQuotas());
+                bar = (ActionCharacterPropertySet)(fmt.ReadObject(xr, true));
+            }
+            Assert.AreEqual(foo.guid, bar.guid);
+            Assert.AreEqual(foo.path.Count, bar.path.Count);
+            for (int i = 0; i < foo.path.Count; i++) {
+                Assert.AreEqual(foo.path[i], bar.path[i]);
+            }
+            Assert.IsNull(bar.from);
+            Assert.IsNotNull(bar.to);
+            Assert.AreEqual((foo.to as CharNumProperty).value, (bar.to as CharNumProperty).value);
+        }
+
+        [TestMethod]
+        public void test_apply_add() {
+            Character somebody = new Character("Somebody");
+            List<String> path = new List<string>() { "XP" };
+            CampaignState state = new CampaignState();
+            Guid chr_guid = state.characters.add_character(somebody);
+            ActionCharacterPropertySet action = new ActionCharacterPropertySet(chr_guid, path, null, new CharNumProperty(123));
+
+            action.apply(state);
+            Assert.IsTrue(somebody.properties.value.ContainsKey("XP"));
+            CharNumProperty prop = somebody.get_property(path) as CharNumProperty;
+            Assert.IsNotNull(prop);
+            Assert.AreEqual(prop.value, 123);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void test_apply_add_no_such_parent() {
+            Character somebody = new Character("Somebody");
+            CampaignState state = new CampaignState();
+            Guid chr_guid = state.characters.add_character(somebody);
+            ActionCharacterPropertySet action = new ActionCharacterPropertySet(chr_guid, new List<string>() { "Skills", "Jump" }, null, new CharNumProperty(123));
+
+            action.apply(state);
+        }
+
+        [TestMethod]
+        public void test_apply_remove() {
+            Character somebody = new Character("Somebody");
+            List<String> path = new List<string>() { "Skills" };
+            somebody.set_property(path, new CharDictProperty());
+            path.Add("Jump");
+            CharNumProperty jump = new CharNumProperty(7);
+            somebody.set_property(path, jump);
+            CampaignState state = new CampaignState();
+            Guid chr_guid = state.characters.add_character(somebody);
+            ActionCharacterPropertySet action = new ActionCharacterPropertySet(chr_guid, path, jump, null);
+
+            action.apply(state);
+            CharDictProperty skills = somebody.properties.value["Skills"] as CharDictProperty;
+            Assert.AreEqual(skills.value.Count, 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void test_apply_remove_no_such_property() {
+            Character somebody = new Character("Somebody");
+            CampaignState state = new CampaignState();
+            Guid chr_guid = state.characters.add_character(somebody);
+            ActionCharacterPropertySet action = new ActionCharacterPropertySet(chr_guid, new List<string>() { "Skills", "Jump" }, new CharNumProperty(123), null);
+
+            action.apply(state);
+        }
+
+        [TestMethod]
+        public void test_apply_update() {
+            Character somebody = new Character("Somebody");
+            List<String> path = new List<string>() { "XP" };
+            CharNumProperty xp = new CharNumProperty(42);
+            somebody.set_property(path, xp);
+            CampaignState state = new CampaignState();
+            Guid chr_guid = state.characters.add_character(somebody);
+            ActionCharacterPropertySet action = new ActionCharacterPropertySet(chr_guid, path, xp, new CharNumProperty(123));
+
+            action.apply(state);
+            Assert.IsTrue(somebody.properties.value.ContainsKey("XP"));
+            CharNumProperty prop = somebody.get_property(path) as CharNumProperty;
+            Assert.IsNotNull(prop);
+            Assert.AreEqual(prop.value, 123);
+        }
+
+        [TestMethod]
+        public void test_revert_add() {
+            Character somebody = new Character("Somebody");
+            CampaignState state = new CampaignState();
+            Guid chr_guid = state.characters.add_character(somebody);
+            ActionCharacterPropertySet action = new ActionCharacterPropertySet(chr_guid, new List<string>() { "XP" }, null, new CharNumProperty(123));
+
+            action.apply(state);
+            action.revert(state);
+            Assert.AreEqual(somebody.properties.value.Count, 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void test_revert_add_no_such_property() {
+            Character somebody = new Character("Somebody");
+            List<string> path = new List<string>() { "XP" };
+            CampaignState state = new CampaignState();
+            Guid chr_guid = state.characters.add_character(somebody);
+            ActionCharacterPropertySet action = new ActionCharacterPropertySet(chr_guid, path, null, new CharNumProperty(123));
+
+            action.apply(state);
+            state.characters.characters[chr_guid].remove_property(path);
+            action.revert(state);
+        }
+
+        [TestMethod]
+        public void test_revert_remove() {
+            Character somebody = new Character("Somebody");
+            List<String> path = new List<string>() { "Skills" };
+            somebody.set_property(path, new CharDictProperty());
+            path.Add("Jump");
+            CharNumProperty jump = new CharNumProperty(7);
+            somebody.set_property(path, jump);
+            CampaignState state = new CampaignState();
+            Guid chr_guid = state.characters.add_character(somebody);
+            ActionCharacterPropertySet action = new ActionCharacterPropertySet(chr_guid, path, jump, null);
+
+            action.apply(state);
+            action.revert(state);
+            CharDictProperty skills = somebody.properties.value["Skills"] as CharDictProperty;
+            Assert.IsTrue(skills.value.ContainsKey("Jump"));
+            CharNumProperty prop = somebody.get_property(path) as CharNumProperty;
+            Assert.IsNotNull(prop);
+            Assert.AreEqual(prop.value, 7);
+        }
+
+        [TestMethod]
+        public void test_revert_update() {
+            Character somebody = new Character("Somebody");
+            List<String> path = new List<string>() { "XP" };
+            CharNumProperty xp = new CharNumProperty(42);
+            somebody.set_property(path, xp);
+            CampaignState state = new CampaignState();
+            Guid chr_guid = state.characters.add_character(somebody);
+            ActionCharacterPropertySet action = new ActionCharacterPropertySet(chr_guid, path, xp, new CharNumProperty(123));
+
+            action.apply(state);
+            action.revert(state);
+            Assert.IsTrue(somebody.properties.value.ContainsKey("XP"));
+            CharNumProperty prop = somebody.get_property(path) as CharNumProperty;
+            Assert.IsNotNull(prop);
+            Assert.AreEqual(prop.value, 42);
+        }
+    }
+
+
+    [TestClass]
+    public class TestActionCharacterPropertyAdjust {
+        [TestMethod]
+        public void test_serialization() {
+            CharNumProperty jump = new CharNumProperty(2);
+            ActionCharacterPropertyAdjust foo = new ActionCharacterPropertyAdjust(Guid.NewGuid(), new List<string>() { "Skills", "Jump" }, null, jump), bar;
+            DataContractSerializer fmt = new DataContractSerializer(typeof(ActionCharacterPropertyAdjust));
+            using (System.IO.MemoryStream ms = new System.IO.MemoryStream()) {
+                fmt.WriteObject(ms, foo);
+                ms.Seek(0, System.IO.SeekOrigin.Begin);
+                System.Xml.XmlDictionaryReader xr = System.Xml.XmlDictionaryReader.CreateTextReader(ms, new System.Xml.XmlDictionaryReaderQuotas());
+                bar = (ActionCharacterPropertyAdjust)(fmt.ReadObject(xr, true));
+            }
+            Assert.AreEqual(foo.guid, bar.guid);
+            Assert.AreEqual(foo.path.Count, bar.path.Count);
+            for (int i = 0; i < foo.path.Count; i++) {
+                Assert.AreEqual(foo.path[i], bar.path[i]);
+            }
+            Assert.IsNull(bar.subtract);
+            Assert.IsNotNull(bar.add);
+            Assert.AreEqual((foo.add as CharNumProperty).value, (bar.add as CharNumProperty).value);
+        }
+
+        [TestMethod]
+        public void test_apply_subtract() {
+            Character somebody = new Character("Somebody");
+            List<String> path = new List<string>() { "XP" };
+            somebody.set_property(path, new CharNumProperty(100));
+            CampaignState state = new CampaignState();
+            Guid chr_guid = state.characters.add_character(somebody);
+            ActionCharacterPropertyAdjust action = new ActionCharacterPropertyAdjust(chr_guid, path, new CharNumProperty(15), null);
+
+            action.apply(state);
+            CharNumProperty prop = somebody.get_property(path) as CharNumProperty;
+            Assert.IsNotNull(prop);
+            Assert.AreEqual(prop.value, 85);
+        }
+
+        [TestMethod]
+        public void test_apply_add() {
+            Character somebody = new Character("Somebody");
+            List<String> path = new List<string>() { "Feats" };
+            CharSetProperty feats = new CharSetProperty(), add_feats = new CharSetProperty();
+            feats.value.Add("Power Attack");
+            add_feats.value.Add("Cleave");
+            somebody.set_property(path, feats);
+            CampaignState state = new CampaignState();
+            Guid chr_guid = state.characters.add_character(somebody);
+            ActionCharacterPropertyAdjust action = new ActionCharacterPropertyAdjust(chr_guid, path, null, add_feats);
+
+            action.apply(state);
+            CharSetProperty prop = somebody.get_property(path) as CharSetProperty;
+            Assert.IsNotNull(prop);
+            Assert.AreEqual(prop.value.Count, 2);
+            Assert.IsTrue(prop.value.Contains("Power Attack"));
+            Assert.IsTrue(prop.value.Contains("Cleave"));
+        }
+
+        [TestMethod]
+        public void test_apply_both() {
+            Character somebody = new Character("Somebody");
+            List<String> path = new List<string>() { "Skills" };
+            somebody.set_property(path, new CharDictProperty());
+            path.Add("Jump");
+            CharNumProperty jump = new CharNumProperty(7), new_jump = new CharNumProperty(10);
+            somebody.set_property(path, jump);
+            CharDictProperty subtract_skills = new CharDictProperty(), add_skills = new CharDictProperty();
+            subtract_skills.value["Jump"] = jump;
+            add_skills.value["Jump"] = new_jump;
+            CampaignState state = new CampaignState();
+            Guid chr_guid = state.characters.add_character(somebody);
+            ActionCharacterPropertyAdjust action = new ActionCharacterPropertyAdjust(chr_guid, new List<string>() { "Skills" }, subtract_skills, add_skills);
+
+            action.apply(state);
+            CharNumProperty prop = somebody.get_property(path) as CharNumProperty;
+            Assert.IsNotNull(prop);
+            Assert.AreEqual(prop.value, 10);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void test_apply_no_such_property() {
+            Character somebody = new Character("Somebody");
+            List<String> path = new List<string>() { "XP" };
+            CampaignState state = new CampaignState();
+            Guid chr_guid = state.characters.add_character(somebody);
+            ActionCharacterPropertyAdjust action = new ActionCharacterPropertyAdjust(chr_guid, path, new CharNumProperty(15), null);
+
+            action.apply(state);
+        }
+
+        [TestMethod]
+        public void test_revert_subtract() {
+            Character somebody = new Character("Somebody");
+            List<String> path = new List<string>() { "XP" };
+            somebody.set_property(path, new CharNumProperty(100));
+            CampaignState state = new CampaignState();
+            Guid chr_guid = state.characters.add_character(somebody);
+            ActionCharacterPropertyAdjust action = new ActionCharacterPropertyAdjust(chr_guid, path, new CharNumProperty(15), null);
+
+            action.apply(state);
+            action.revert(state);
+            CharNumProperty prop = somebody.get_property(path) as CharNumProperty;
+            Assert.IsNotNull(prop);
+            Assert.AreEqual(prop.value, 100);
+        }
+
+        [TestMethod]
+        public void test_revert_add() {
+            Character somebody = new Character("Somebody");
+            List<String> path = new List<string>() { "Feats" };
+            CharSetProperty feats = new CharSetProperty(), add_feats = new CharSetProperty();
+            feats.value.Add("Power Attack");
+            add_feats.value.Add("Cleave");
+            somebody.set_property(path, feats);
+            CampaignState state = new CampaignState();
+            Guid chr_guid = state.characters.add_character(somebody);
+            ActionCharacterPropertyAdjust action = new ActionCharacterPropertyAdjust(chr_guid, path, null, add_feats);
+
+            action.apply(state);
+            action.revert(state);
+            CharSetProperty prop = somebody.get_property(path) as CharSetProperty;
+            Assert.IsNotNull(prop);
+            Assert.AreEqual(prop.value.Count, 1);
+            Assert.IsTrue(prop.value.Contains("Power Attack"));
+        }
+
+        [TestMethod]
+        public void test_revert_both() {
+            Character somebody = new Character("Somebody");
+            List<String> path = new List<string>() { "Skills" };
+            somebody.set_property(path, new CharDictProperty());
+            path.Add("Jump");
+            CharNumProperty jump = new CharNumProperty(7), new_jump = new CharNumProperty(10);
+            somebody.set_property(path, jump);
+            CharDictProperty subtract_skills = new CharDictProperty(), add_skills = new CharDictProperty();
+            subtract_skills.value["Jump"] = jump;
+            add_skills.value["Jump"] = new_jump;
+            CampaignState state = new CampaignState();
+            Guid chr_guid = state.characters.add_character(somebody);
+            ActionCharacterPropertyAdjust action = new ActionCharacterPropertyAdjust(chr_guid, new List<string>() { "Skills" }, subtract_skills, add_skills);
+
+            action.apply(state);
+            action.revert(state);
+            CharNumProperty prop = somebody.get_property(path) as CharNumProperty;
+            Assert.IsNotNull(prop);
+            Assert.AreEqual(prop.value, 7);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void test_revert_no_such_property() {
+            Character somebody = new Character("Somebody");
+            List<String> path = new List<string>() { "XP" };
+            CampaignState state = new CampaignState();
+            Guid chr_guid = state.characters.add_character(somebody);
+            ActionCharacterPropertyAdjust action = new ActionCharacterPropertyAdjust(chr_guid, path, new CharNumProperty(15), null);
+
+            action.revert(state);
+        }
+    }
 }
