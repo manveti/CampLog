62a345364007370e4f57129bc72df139e009aecc
manveti <manveti@users.noreply.github.com>
2021-03-23 19:12:30 -0700

    Add note actions.

---
+++ CampLog/NoteAction.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CampLog {
+    [Serializable]
+    public class ActionNoteCreate : EventAction {
+        public readonly Guid guid;
+        public readonly Note note;
+
+        public override string description { get => "Add note"; }
+
+        public ActionNoteCreate(Guid guid, Note note) {
+            if (note is null) { throw new ArgumentNullException(nameof(note)); }
+            this.guid = guid;
+            this.note = note.copy();
+        }
+
+        public override void apply(CampaignState state, Event evt) {
+            state.notes.add_note(this.note.copy(), this.guid);
+        }
+
+        public override void revert(CampaignState state, Event evt) {
+            state.notes.remove_note(this.guid);
+            state.notes.notes.Remove(this.guid);
+        }
+    }
+
+
+    [Serializable]
+    public class ActionNoteRemove : EventAction {
+        public readonly Guid guid;
+
+        public override string description { get => "Remove note"; }
+
+        public ActionNoteRemove(Guid guid) {
+            this.guid = guid;
+        }
+
+        public override void apply(CampaignState state, Event evt) {
+            state.notes.remove_note(this.guid);
+        }
+
+        public override void revert(CampaignState state, Event evt) {
+            state.notes.restore_note(this.guid);
+        }
+    }
+
+
+    [Serializable]
+    public class ActionNoteRestore : EventAction {
+        public readonly Guid guid;
+
+        public override string description { get => "Restore note"; }
+
+        public ActionNoteRestore(Guid guid) {
+            this.guid = guid;
+        }
+
+        public override void apply(CampaignState state, Event evt) {
+            state.notes.restore_note(this.guid);
+        }
+
+        public override void revert(CampaignState state, Event evt) {
+            state.notes.remove_note(this.guid);
+        }
+    }
+
+
+    [Serializable]
+    public class ActionNoteUpdate : EventAction {
+        public readonly Guid guid;
+        public readonly string contents_from;
+        public readonly string contents_to;
+        public readonly HashSet<Guid> remove_topics;
+        public readonly HashSet<Guid> add_topics;
+
+        public override string description { get => "Update note"; }
+
+        public ActionNoteUpdate(Guid guid, string contents_from, string contents_to, HashSet<Guid> remove_topics, HashSet<Guid> add_topics) {
+            if ((contents_from is null) != (contents_to is null)) { throw new ArgumentNullException(nameof(contents_from)); }
+            if ((contents_to is null) && (remove_topics is null) && (add_topics is null)) { throw new ArgumentNullException(nameof(contents_to)); }
+            this.guid = guid;
+            this.contents_from = contents_from;
+            this.contents_to = contents_to;
+            this.remove_topics = remove_topics;
+            this.add_topics = add_topics;
+        }
+
+        public override void apply(CampaignState state, Event evt) {
+            if (!state.notes.notes.ContainsKey(this.guid)) { throw new ArgumentOutOfRangeException(); }
+            Note note = state.notes.notes[this.guid];
+            if (this.contents_to is not null) { note.contents = this.contents_to; }
+            if (this.remove_topics is not null) { note.topics.ExceptWith(this.remove_topics); }
+            if (this.add_topics is not null) { note.topics.UnionWith(this.add_topics); }
+        }
+
+        public override void revert(CampaignState state, Event evt) {
+            if (!state.notes.notes.ContainsKey(this.guid)) { throw new ArgumentOutOfRangeException(); }
+            Note note = state.notes.notes[this.guid];
+            if (this.contents_from is not null) { note.contents = this.contents_from; }
+            if (this.add_topics is not null) { note.topics.ExceptWith(this.add_topics); }
+            if (this.remove_topics is not null) { note.topics.UnionWith(this.remove_topics); }
+        }
+    }
+}
---
+++ CampLogTest/TestNoteAction.cs
@@ -0,0 +1,290 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+using CampLog;
+
+namespace CampLogTest {
+    [TestClass]
+    public class TestActionNoteCreate {
+        [TestMethod]
+        public void test_serialization() {
+            Note note = new Note("Some note", Guid.NewGuid());
+            ActionNoteCreate foo = new ActionNoteCreate(Guid.NewGuid(), note), bar;
+            DataContractSerializer fmt = new DataContractSerializer(typeof(ActionNoteCreate));
+            using (System.IO.MemoryStream ms = new System.IO.MemoryStream()) {
+                fmt.WriteObject(ms, foo);
+                ms.Seek(0, System.IO.SeekOrigin.Begin);
+                System.Xml.XmlDictionaryReader xr = System.Xml.XmlDictionaryReader.CreateTextReader(ms, new System.Xml.XmlDictionaryReaderQuotas());
+                bar = (ActionNoteCreate)(fmt.ReadObject(xr, true));
+            }
+            Assert.AreEqual(foo.guid, bar.guid);
+            Assert.AreEqual(foo.note.contents, bar.note.contents);
+        }
+
+        [TestMethod]
+        public void test_apply() {
+            Event evt = new Event(42, DateTime.Now, "Some Event");
+            Note note = new Note("Some note", evt.guid);
+            Guid note_guid = Guid.NewGuid();
+            ActionNoteCreate action = new ActionNoteCreate(note_guid, note);
+            CampaignState state = new CampaignState();
+
+            action.apply(state, evt);
+            Assert.AreEqual(state.notes.notes.Count, 1);
+            Assert.IsTrue(state.notes.notes.ContainsKey(note_guid));
+            Assert.AreEqual(state.notes.notes[note_guid].contents, "Some note");
+            Assert.IsFalse(ReferenceEquals(state.notes.notes[note_guid], note));
+            Assert.AreEqual(state.notes.active_notes.Count, 1);
+            Assert.IsTrue(state.notes.active_notes.Contains(note_guid));
+        }
+
+        [TestMethod]
+        public void test_revert() {
+            Event evt = new Event(42, DateTime.Now, "Some Event");
+            Note note = new Note("Some note", evt.guid);
+            Guid note_guid = Guid.NewGuid();
+            ActionNoteCreate action = new ActionNoteCreate(note_guid, note);
+            CampaignState state = new CampaignState();
+
+            action.apply(state, evt);
+            action.revert(state, evt);
+            Assert.AreEqual(state.notes.notes.Count, 0);
+            Assert.AreEqual(state.notes.active_notes.Count, 0);
+        }
+    }
+
+
+    [TestClass]
+    public class TestActionNoteRemove {
+        [TestMethod]
+        public void test_serialization() {
+            ActionNoteRemove foo = new ActionNoteRemove(Guid.NewGuid()), bar;
+            DataContractSerializer fmt = new DataContractSerializer(typeof(ActionNoteRemove));
+            using (System.IO.MemoryStream ms = new System.IO.MemoryStream()) {
+                fmt.WriteObject(ms, foo);
+                ms.Seek(0, System.IO.SeekOrigin.Begin);
+                System.Xml.XmlDictionaryReader xr = System.Xml.XmlDictionaryReader.CreateTextReader(ms, new System.Xml.XmlDictionaryReaderQuotas());
+                bar = (ActionNoteRemove)(fmt.ReadObject(xr, true));
+            }
+            Assert.AreEqual(foo.guid, bar.guid);
+        }
+
+        [TestMethod]
+        public void test_apply() {
+            Event evt = new Event(42, DateTime.Now, "Some Event");
+            Note note = new Note("Some note", evt.guid);
+            CampaignState state = new CampaignState();
+            Guid note_guid = state.notes.add_note(note);
+            ActionNoteRemove action = new ActionNoteRemove(note_guid);
+
+            action.apply(state, evt);
+            Assert.AreEqual(state.notes.notes.Count, 1);
+            Assert.IsTrue(state.notes.notes.ContainsKey(note_guid));
+            Assert.AreEqual(state.notes.notes[note_guid].contents, "Some note");
+            Assert.AreEqual(state.notes.active_notes.Count, 0);
+        }
+
+        [TestMethod]
+        public void test_revert() {
+            Event evt = new Event(42, DateTime.Now, "Some Event");
+            Note note = new Note("Some note", evt.guid);
+            CampaignState state = new CampaignState();
+            Guid note_guid = state.notes.add_note(note);
+            ActionNoteRemove action = new ActionNoteRemove(note_guid);
+
+            action.apply(state, evt);
+            action.revert(state, evt);
+            Assert.AreEqual(state.notes.notes.Count, 1);
+            Assert.IsTrue(state.notes.notes.ContainsKey(note_guid));
+            Assert.AreEqual(state.notes.notes[note_guid].contents, "Some note");
+            Assert.AreEqual(state.notes.active_notes.Count, 1);
+            Assert.IsTrue(state.notes.active_notes.Contains(note_guid));
+        }
+    }
+
+
+    [TestClass]
+    public class TestActionNoteRestore {
+        [TestMethod]
+        public void test_serialization() {
+            ActionNoteRestore foo = new ActionNoteRestore(Guid.NewGuid()), bar;
+            DataContractSerializer fmt = new DataContractSerializer(typeof(ActionNoteRestore));
+            using (System.IO.MemoryStream ms = new System.IO.MemoryStream()) {
+                fmt.WriteObject(ms, foo);
+                ms.Seek(0, System.IO.SeekOrigin.Begin);
+                System.Xml.XmlDictionaryReader xr = System.Xml.XmlDictionaryReader.CreateTextReader(ms, new System.Xml.XmlDictionaryReaderQuotas());
+                bar = (ActionNoteRestore)(fmt.ReadObject(xr, true));
+            }
+            Assert.AreEqual(foo.guid, bar.guid);
+        }
+
+        [TestMethod]
+        public void test_apply() {
+            Event evt = new Event(42, DateTime.Now, "Some Event");
+            Note note = new Note("Some note", evt.guid);
+            CampaignState state = new CampaignState();
+            Guid note_guid = state.notes.add_note(note);
+            ActionNoteRestore action = new ActionNoteRestore(note_guid);
+
+            state.notes.remove_note(note_guid);
+
+            action.apply(state, evt);
+            Assert.AreEqual(state.notes.notes.Count, 1);
+            Assert.IsTrue(state.notes.notes.ContainsKey(note_guid));
+            Assert.AreEqual(state.notes.notes[note_guid].contents, "Some note");
+            Assert.AreEqual(state.notes.active_notes.Count, 1);
+            Assert.IsTrue(state.notes.active_notes.Contains(note_guid));
+        }
+
+        [TestMethod]
+        public void test_revert() {
+            Event evt = new Event(42, DateTime.Now, "Some Event");
+            Note note = new Note("Some note", evt.guid);
+            CampaignState state = new CampaignState();
+            Guid note_guid = state.notes.add_note(note);
+            ActionNoteRestore action = new ActionNoteRestore(note_guid);
+
+            state.notes.remove_note(note_guid);
+
+            action.apply(state, evt);
+            action.revert(state, evt);
+            Assert.AreEqual(state.notes.notes.Count, 1);
+            Assert.IsTrue(state.notes.notes.ContainsKey(note_guid));
+            Assert.AreEqual(state.notes.notes[note_guid].contents, "Some note");
+            Assert.AreEqual(state.notes.active_notes.Count, 0);
+        }
+    }
+
+
+    [TestClass]
+    public class TestActionNoteUpdate {
+        [TestMethod]
+        public void test_serialization() {
+            HashSet<Guid> rem_topics = new HashSet<Guid>() { Guid.NewGuid() }, add_topics = new HashSet<Guid>() { Guid.NewGuid() };
+            ActionNoteUpdate foo = new ActionNoteUpdate(Guid.NewGuid(), "Old note", "New Note", rem_topics, add_topics), bar;
+            DataContractSerializer fmt = new DataContractSerializer(typeof(ActionNoteUpdate));
+            using (System.IO.MemoryStream ms = new System.IO.MemoryStream()) {
+                fmt.WriteObject(ms, foo);
+                ms.Seek(0, System.IO.SeekOrigin.Begin);
+                System.Xml.XmlDictionaryReader xr = System.Xml.XmlDictionaryReader.CreateTextReader(ms, new System.Xml.XmlDictionaryReaderQuotas());
+                bar = (ActionNoteUpdate)(fmt.ReadObject(xr, true));
+            }
+            Assert.AreEqual(foo.guid, bar.guid);
+            Assert.AreEqual(foo.contents_from, bar.contents_from);
+            Assert.AreEqual(foo.contents_to, bar.contents_to);
+            Assert.AreEqual(foo.remove_topics.Count, bar.remove_topics.Count);
+            foreach (Guid topic in foo.remove_topics) {
+                Assert.IsTrue(bar.remove_topics.Contains(topic));
+            }
+            Assert.AreEqual(foo.add_topics.Count, bar.add_topics.Count);
+            foreach (Guid topic in foo.add_topics) {
+                Assert.IsTrue(bar.add_topics.Contains(topic));
+            }
+        }
+
+        [TestMethod]
+        public void test_apply() {
+            Event evt = new Event(42, DateTime.Now, "Some Event");
+            Guid topic1 = Guid.NewGuid(), topic2 = Guid.NewGuid(), topic3 = Guid.NewGuid();
+            HashSet<Guid> rem_topics = new HashSet<Guid>() { topic2 }, add_topics = new HashSet<Guid>() { topic3 };
+            Note note = new Note("Some note", evt.guid, new HashSet<Guid>() { topic1, topic2 });
+            CampaignState state = new CampaignState();
+            Guid note_guid = state.notes.add_note(note);
+            ActionNoteUpdate action = new ActionNoteUpdate(note_guid, "Some note", "New note", rem_topics, add_topics);
+
+            action.apply(state, evt);
+            Assert.AreEqual(note.contents, "New note");
+            Assert.AreEqual(note.topics.Count, 2);
+            Assert.IsTrue(note.topics.Contains(topic1));
+            Assert.IsTrue(note.topics.Contains(topic3));
+        }
+
+        [TestMethod]
+        public void test_apply_contents_only() {
+            Event evt = new Event(42, DateTime.Now, "Some Event");
+            Guid topic1 = Guid.NewGuid(), topic2 = Guid.NewGuid();
+            Note note = new Note("Some note", evt.guid, new HashSet<Guid>() { topic1, topic2 });
+            CampaignState state = new CampaignState();
+            Guid note_guid = state.notes.add_note(note);
+            ActionNoteUpdate action = new ActionNoteUpdate(note_guid, "Some note", "New note", null, null);
+
+            action.apply(state, evt);
+            Assert.AreEqual(note.contents, "New note");
+            Assert.AreEqual(note.topics.Count, 2);
+            Assert.IsTrue(note.topics.Contains(topic1));
+            Assert.IsTrue(note.topics.Contains(topic2));
+        }
+
+        [TestMethod]
+        public void test_apply_topics_only() {
+            Event evt = new Event(42, DateTime.Now, "Some Event");
+            Guid topic1 = Guid.NewGuid(), topic2 = Guid.NewGuid(), topic3 = Guid.NewGuid();
+            HashSet<Guid> rem_topics = new HashSet<Guid>() { topic2 }, add_topics = new HashSet<Guid>() { topic3 };
+            Note note = new Note("Some note", evt.guid, new HashSet<Guid>() { topic1, topic2 });
+            CampaignState state = new CampaignState();
+            Guid note_guid = state.notes.add_note(note);
+            ActionNoteUpdate action = new ActionNoteUpdate(note_guid, null, null, rem_topics, add_topics);
+
+            action.apply(state, evt);
+            Assert.AreEqual(note.contents, "Some note");
+            Assert.AreEqual(note.topics.Count, 2);
+            Assert.IsTrue(note.topics.Contains(topic1));
+            Assert.IsTrue(note.topics.Contains(topic3));
+        }
+
+        [TestMethod]
+        public void test_revert() {
+            Event evt = new Event(42, DateTime.Now, "Some Event");
+            Guid topic1 = Guid.NewGuid(), topic2 = Guid.NewGuid(), topic3 = Guid.NewGuid();
+            HashSet<Guid> rem_topics = new HashSet<Guid>() { topic2 }, add_topics = new HashSet<Guid>() { topic3 };
+            Note note = new Note("Some note", evt.guid, new HashSet<Guid>() { topic1, topic2 });
+            CampaignState state = new CampaignState();
+            Guid note_guid = state.notes.add_note(note);
+            ActionNoteUpdate action = new ActionNoteUpdate(note_guid, "Some note", "New note", rem_topics, add_topics);
+
+            action.apply(state, evt);
+            action.revert(state, evt);
+            Assert.AreEqual(note.contents, "Some note");
+            Assert.AreEqual(note.topics.Count, 2);
+            Assert.IsTrue(note.topics.Contains(topic1));
+            Assert.IsTrue(note.topics.Contains(topic2));
+        }
+
+        [TestMethod]
+        public void test_revert_contents_only() {
+            Event evt = new Event(42, DateTime.Now, "Some Event");
+            Guid topic1 = Guid.NewGuid(), topic2 = Guid.NewGuid();
+            Note note = new Note("Some note", evt.guid, new HashSet<Guid>() { topic1, topic2 });
+            CampaignState state = new CampaignState();
+            Guid note_guid = state.notes.add_note(note);
+            ActionNoteUpdate action = new ActionNoteUpdate(note_guid, "Some note", "New note", null, null);
+
+            action.apply(state, evt);
+            action.revert(state, evt);
+            Assert.AreEqual(note.contents, "Some note");
+            Assert.AreEqual(note.topics.Count, 2);
+            Assert.IsTrue(note.topics.Contains(topic1));
+            Assert.IsTrue(note.topics.Contains(topic2));
+        }
+
+        [TestMethod]
+        public void test_revert_topics_only() {
+            Event evt = new Event(42, DateTime.Now, "Some Event");
+            Guid topic1 = Guid.NewGuid(), topic2 = Guid.NewGuid(), topic3 = Guid.NewGuid();
+            HashSet<Guid> rem_topics = new HashSet<Guid>() { topic2 }, add_topics = new HashSet<Guid>() { topic3 };
+            Note note = new Note("Some note", evt.guid, new HashSet<Guid>() { topic1, topic2 });
+            CampaignState state = new CampaignState();
+            Guid note_guid = state.notes.add_note(note);
+            ActionNoteUpdate action = new ActionNoteUpdate(note_guid, null, null, rem_topics, add_topics);
+
+            action.apply(state, evt);
+            action.revert(state, evt);
+            Assert.AreEqual(note.contents, "Some note");
+            Assert.AreEqual(note.topics.Count, 2);
+            Assert.IsTrue(note.topics.Contains(topic1));
+            Assert.IsTrue(note.topics.Contains(topic2));
+        }
+    }
+}
