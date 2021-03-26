c2c2192fe162b13eb4fd90482721e1e80dd52a06
manveti <manveti@users.noreply.github.com>
2021-03-26 01:54:12 -0700

    Add RefcountSet class. This class acts like a HashSet, but maintains a counter of how many times each item has been added. While Remove and ExceptWith function normally, there are new RemoveRef and ExceptRefWith companions which decrement the refcount and only remove the item if the refcount drops to 0. This will help cases like CharSetProperty.value, where two actions could add the same item to the set -- reverting only the second action will curently remove the item, but it should actually still be there until the first action is reverted too. By switching from a HashSet to a RefcountSet there, we'll be able to handle that case properly.

---
+++ CampLog/Util.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace CampLog {
+    [DataContract]
+    public class RefcountSet<T> : IEnumerable<T> {
+        [DataMember] public Dictionary<T, int> contents;
+        public int Count { get => this.contents.Count; }
+
+        public RefcountSet() {
+            this.contents = new Dictionary<T, int>();
+        }
+
+        public RefcountSet(Dictionary<T, int> contents) {
+            this.contents = new Dictionary<T, int>(contents);
+        }
+
+        public RefcountSet(IEnumerable<T> contents) {
+            this.contents = new Dictionary<T, int>();
+            foreach (T item in contents) {
+                this.contents[item] = 1;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator() => this.contents.Keys.GetEnumerator();
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() => this.contents.Keys.GetEnumerator();
+
+        public bool Add(T item) {
+            if (this.contents.ContainsKey(item)) {
+                this.contents[item] += 1;
+                return false;
+            }
+            this.contents[item] = 1;
+            return true;
+        }
+
+        public void Clear() => this.contents.Clear();
+
+        public bool Contains(T item) => this.contents.ContainsKey(item);
+
+        public void ExceptWith(IEnumerable<T> other) {
+            foreach (T item in other) {
+                this.contents.Remove(item);
+            }
+        }
+
+        public void ExceptRefWith(IEnumerable<T> other) {
+            foreach (T item in other) {
+                this.RemoveRef(item);
+            }
+        }
+
+        public bool Remove(T item) => this.contents.Remove(item);
+
+        public bool RemoveRef(T item) {
+            if (!this.contents.ContainsKey(item)) { return false; }
+            this.contents[item] -= 1;
+            if (this.contents[item] <= 0) { this.contents.Remove(item); }
+            return true;
+        }
+
+        public void UnionWith(IEnumerable<T> other) {
+            foreach (T item in other) {
+                this.Add(item);
+            }
+        }
+    }
+}
---
+++ CampLogTest/TestUtil.cs
@@ -0,0 +1,218 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+using CampLog;
+
+namespace CampLogTest {
+    [TestClass]
+    public class TestRefcountSet {
+        [TestMethod]
+        public void test_serialization() {
+            RefcountSet<int> foo = new RefcountSet<int>() { 1, 2, 3 }, bar;
+
+            foo.Add(3);
+
+            DataContractSerializer fmt = new DataContractSerializer(typeof(RefcountSet<int>));
+            using (System.IO.MemoryStream ms = new System.IO.MemoryStream()) {
+                fmt.WriteObject(ms, foo);
+                ms.Seek(0, System.IO.SeekOrigin.Begin);
+                System.Xml.XmlDictionaryReader xr = System.Xml.XmlDictionaryReader.CreateTextReader(ms, new System.Xml.XmlDictionaryReaderQuotas());
+                bar = (RefcountSet<int>)(fmt.ReadObject(xr, true));
+            }
+            Assert.AreEqual(foo.Count, bar.Count);
+            Assert.AreEqual(foo.contents.Count, bar.contents.Count);
+            foreach (int i in foo.contents.Keys) {
+                Assert.IsTrue(bar.contents.ContainsKey(i));
+                Assert.AreEqual(foo.contents[i], bar.contents[i]);
+            }
+        }
+
+        [TestMethod]
+        public void test_constructor_dict() {
+            RefcountSet<int> foo = new RefcountSet<int>() { 1, 2, 3 }, bar;
+
+            foo.Add(3);
+
+            bar = new RefcountSet<int>(foo.contents);
+            Assert.AreEqual(foo.Count, bar.Count);
+            Assert.IsFalse(ReferenceEquals(foo.contents, bar.contents));
+            Assert.AreEqual(foo.contents.Count, bar.contents.Count);
+            foreach (int i in foo.contents.Keys) {
+                Assert.IsTrue(bar.contents.ContainsKey(i));
+                Assert.AreEqual(foo.contents[i], bar.contents[i]);
+            }
+        }
+
+        [TestMethod]
+        public void test_constructor_set() {
+            HashSet<int> foo = new HashSet<int>() { 1, 2, 3 };
+            RefcountSet<int> bar = new RefcountSet<int>(foo);
+            Assert.AreEqual(foo.Count, bar.Count);
+            Assert.IsFalse(ReferenceEquals(foo, bar.contents));
+            Assert.AreEqual(foo.Count, bar.contents.Count);
+            foreach (int i in foo) {
+                Assert.IsTrue(bar.contents.ContainsKey(i));
+                Assert.AreEqual(bar.contents[i], 1);
+            }
+        }
+
+        [TestMethod]
+        public void test_constructor_refcount_set() {
+            RefcountSet<int> foo = new RefcountSet<int>() { 1, 2, 3 }, bar;
+
+            foo.Add(3);
+
+            bar = new RefcountSet<int>(foo);
+            Assert.AreEqual(foo.Count, bar.Count);
+            Assert.IsFalse(ReferenceEquals(foo.contents, bar.contents));
+            Assert.AreEqual(foo.contents.Count, bar.contents.Count);
+            foreach (int i in foo.contents.Keys) {
+                Assert.IsTrue(bar.contents.ContainsKey(i));
+                Assert.AreEqual(bar.contents[i], 1);
+            }
+        }
+
+        [TestMethod]
+        public void test_add() {
+            RefcountSet<string> foo = new RefcountSet<string>() { "foo", "bar" };
+            foo.Add("baz");
+            Assert.AreEqual(foo.Count, 3);
+            Assert.IsTrue(foo.contents.ContainsKey("baz"));
+            Assert.AreEqual(foo.contents["baz"], 1);
+        }
+
+        [TestMethod]
+        public void test_add_increment() {
+            RefcountSet<string> foo = new RefcountSet<string>() { "foo", "bar", "baz" };
+            foo.Add("baz");
+            Assert.AreEqual(foo.Count, 3);
+            Assert.IsTrue(foo.contents.ContainsKey("baz"));
+            Assert.AreEqual(foo.contents["baz"], 2);
+        }
+
+        [TestMethod]
+        public void test_clear() {
+            RefcountSet<int> foo = new RefcountSet<int>() { 1, 2, 3 };
+            foo.Clear();
+            Assert.AreEqual(foo.Count, 0);
+            Assert.AreEqual(foo.contents.Count, 0);
+        }
+
+        [TestMethod]
+        public void test_contains() {
+            RefcountSet<int> foo = new RefcountSet<int>() { 1, 2, 3 };
+            foo.Add(3);
+            Assert.IsTrue(foo.Contains(1));
+            Assert.IsTrue(foo.Contains(2));
+            Assert.IsTrue(foo.Contains(3));
+            Assert.IsFalse(foo.Contains(4));
+        }
+
+        [TestMethod]
+        public void test_except_with() {
+            RefcountSet<int> foo = new RefcountSet<int>() { 1, 2, 3 };
+            HashSet<int> bar = new HashSet<int>() { 2, 3, 4 };
+
+            foo.Add(3);
+
+            foo.ExceptWith(bar);
+            Assert.AreEqual(foo.Count, 1);
+            Assert.IsTrue(foo.Contains(1));
+            Assert.IsFalse(foo.Contains(2));
+            Assert.IsFalse(foo.Contains(3));
+            Assert.IsFalse(foo.Contains(4));
+        }
+
+        [TestMethod]
+        public void test_except_with_refcount_set() {
+            RefcountSet<int> foo = new RefcountSet<int>() { 1, 2, 3 }, bar = new RefcountSet<int>() { 2, 3, 4 };
+
+            foo.Add(3);
+            bar.Add(3);
+
+            foo.ExceptWith(bar);
+            Assert.AreEqual(foo.Count, 1);
+            Assert.IsTrue(foo.Contains(1));
+            Assert.IsFalse(foo.Contains(2));
+            Assert.IsFalse(foo.Contains(3));
+            Assert.IsFalse(foo.Contains(4));
+        }
+
+        [TestMethod]
+        public void test_except_ref_with() {
+            RefcountSet<int> foo = new RefcountSet<int>() { 1, 2, 3 }, bar = new RefcountSet<int>() { 2, 3, 4 };
+
+            foo.Add(3);
+            bar.Add(3);
+
+            foo.ExceptRefWith(bar);
+            Assert.AreEqual(foo.Count, 2);
+            Assert.IsTrue(foo.Contains(1));
+            Assert.IsFalse(foo.Contains(2));
+            Assert.IsTrue(foo.Contains(3));
+            Assert.IsFalse(foo.Contains(4));
+        }
+
+        [TestMethod]
+        public void test_remove() {
+            RefcountSet<int> foo = new RefcountSet<int>() { 1, 2, 3 };
+
+            foo.Add(3);
+
+            foo.Remove(2);
+            foo.Remove(3);
+            Assert.AreEqual(foo.Count, 1);
+            Assert.IsTrue(foo.Contains(1));
+            Assert.IsFalse(foo.Contains(2));
+            Assert.IsFalse(foo.Contains(3));
+            Assert.IsFalse(foo.Contains(4));
+        }
+
+        [TestMethod]
+        public void test_remove_ref() {
+            RefcountSet<int> foo = new RefcountSet<int>() { 1, 2, 3 };
+
+            foo.Add(3);
+
+            foo.RemoveRef(2);
+            foo.RemoveRef(3);
+            Assert.AreEqual(foo.Count, 2);
+            Assert.IsTrue(foo.Contains(1));
+            Assert.IsFalse(foo.Contains(2));
+            Assert.IsTrue(foo.Contains(3));
+            Assert.IsFalse(foo.Contains(4));
+        }
+
+        [TestMethod]
+        public void test_union_with() {
+            RefcountSet<int> foo = new RefcountSet<int>() { 1, 2, 3 };
+            HashSet<int> bar = new HashSet<int>() { 2, 3, 4 };
+
+            foo.Add(3);
+
+            foo.UnionWith(bar);
+            Assert.AreEqual(foo.Count, 4);
+            Assert.AreEqual(foo.contents[1], 1);
+            Assert.AreEqual(foo.contents[2], 2);
+            Assert.AreEqual(foo.contents[3], 3);
+            Assert.AreEqual(foo.contents[4], 1);
+        }
+
+        [TestMethod]
+        public void test_union_with_refcount_set() {
+            RefcountSet<int> foo = new RefcountSet<int>() { 1, 2, 3 }, bar = new RefcountSet<int>() { 2, 3, 4 };
+
+            foo.Add(3);
+            bar.Add(3);
+
+            foo.UnionWith(bar);
+            Assert.AreEqual(foo.Count, 4);
+            Assert.AreEqual(foo.contents[1], 1);
+            Assert.AreEqual(foo.contents[2], 2);
+            Assert.AreEqual(foo.contents[3], 3);
+            Assert.AreEqual(foo.contents[4], 1);
+        }
+    }
+}
