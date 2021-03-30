e6de16651fd7184747cbf72466ff86d74b1b5528
manveti <manveti@users.noreply.github.com>
2021-03-30 01:50:20 -0700

    Add methods to get previous states and to add/remove log entries.

--- CampLog/Campaign.cs
+++ CampLog/Campaign.cs
@@ -48,5 +48,67 @@ namespace CampLog {
             this.topics = new Dictionary<Guid, Topic>();
             this.notes = new List<ExternalNote>();
         }
+
+        public CampaignState get_entry_state(int idx) {
+            if ((idx < -1) || (idx >= this.valid_entries)) { throw new ArgumentOutOfRangeException(nameof(idx)); }
+            CampaignState result = this.state.copy();
+            for (int i = this.valid_entries - 1; i > idx; i--) {
+                this.entries[i].revert(result);
+            }
+            return result;
+        }
+
+        public int get_timestamp_index(decimal timestamp) {
+            // search with maximum real-world timestamp so we get all entries for the given in-game timestamp
+            // NOTE: not doing the same with the maximum Guid: we have bigger problems if we have entries at the maximum DateTime
+            Entry ent = new Entry(timestamp, DateTime.MaxValue, "");
+            int idx = this.entries.BinarySearch(ent);
+            if (idx >= 0) { return idx; }
+            // ~idx is insertion index, but we want index of last entry before timestamp, so subtract 1
+            return ~idx - 1;
+        }
+
+        public CampaignState get_timestamp_state(decimal timestamp) => this.get_entry_state(this.get_timestamp_index(timestamp));
+
+        private void revert_state(int idx) {
+            for (; this.valid_entries - 1 > idx; this.valid_entries--) {
+                this.entries[this.valid_entries - 1].revert(this.state);
+            }
+        }
+
+        private void advance_state() {
+            for (; this.valid_entries < this.entries.Count; this.valid_entries++) {
+                try {
+                    this.entries[this.valid_entries].rebase(this.state);
+                }
+                catch (ArgumentException) { break; }
+                try {
+                    this.entries[this.valid_entries].apply(this.state);
+                }
+                catch (ArgumentException e) {
+                    this.entries[this.valid_entries].revert(this.state, (e.Data["action_index"] as int?) ?? 0 - 1);
+                    break;
+                }
+            }
+        }
+
+        public void add_entry(Entry ent) {
+            int idx = this.entries.BinarySearch(ent);
+            if (idx >= 0) { throw new ArgumentOutOfRangeException(nameof(ent)); }
+            idx = ~idx;
+            this.revert_state(idx - 1);
+            this.entries.Insert(idx, ent);
+            if (this.valid_entries < idx) { return; }
+            this.advance_state();
+        }
+
+        public void remove_entry(int idx, bool invalidate = false) {
+            if ((idx < 0) || (idx >= this.entries.Count)) { throw new ArgumentOutOfRangeException(nameof(idx)); }
+            this.revert_state(idx - 1);
+            this.entries.RemoveAt(idx);
+            if (this.valid_entries < idx) { return; }
+            if (invalidate) { return; }
+            this.advance_state();
+        }
     }
 }
--- CampLogTest/TestCampaign.cs
+++ CampLogTest/TestCampaign.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 using System.Runtime.Serialization;
 
 using CampLog;
@@ -156,6 +157,225 @@ namespace CampLogTest {
             for (int i = 0; i < foo.notes.Count; i++) {
                 Assert.AreEqual(foo.notes[i].contents, bar.notes[i].contents);
             }
+        }
+
+        [TestMethod]
+        public void test_get_entry_state() {
+            Character c1 = new Character("Somebody"), c2 = new Character("Someone Else");
+            Guid c1_guid = Guid.NewGuid(), c2_guid = Guid.NewGuid();
+            ActionCharacterSet a1 = new ActionCharacterSet(c1_guid, null, c1), a2 = new ActionCharacterSet(c2_guid, null, c2),
+                a3 = new ActionCharacterSet(c1_guid, c1, null);
+            Entry e1 = new Entry(10, new DateTime(1), "Somebody joined party", actions: new List<EntryAction>() { a1 }),
+                e2 = new Entry(20, new DateTime(2), "Someone Else joined party", actions: new List<EntryAction>() { a2 }),
+                e3 = new Entry(30, new DateTime(3), "Sombody left party", actions: new List<EntryAction>() { a3 });
+            CampaignDomain domain = new CampaignDomain() { entries = new List<Entry>() { e1, e2, e3 } };
+
+            e1.apply(domain.state);
+            e2.apply(domain.state);
+            e3.apply(domain.state);
+            domain.valid_entries = 3;
+
+            CampaignState state = domain.get_entry_state(1);
+            Assert.IsFalse(ReferenceEquals(state, domain.state));
+            Assert.AreEqual(state.characters.characters.Count, 2);
+            Assert.IsTrue(state.characters.characters.ContainsKey(c1_guid));
+            Assert.IsTrue(state.characters.characters.ContainsKey(c2_guid));
+            Assert.AreEqual(state.characters.active_characters.Count, 2);
+            Assert.IsTrue(state.characters.active_characters.Contains(c1_guid));
+            Assert.IsTrue(state.characters.active_characters.Contains(c2_guid));
+        }
+
+        [TestMethod]
+        public void test_get_timestamp_index() {
+            Character c1 = new Character("Somebody"), c2 = new Character("Someone Else");
+            Guid c1_guid = Guid.NewGuid(), c2_guid = Guid.NewGuid();
+            ActionCharacterSet a1 = new ActionCharacterSet(c1_guid, null, c1), a2 = new ActionCharacterSet(c2_guid, null, c2),
+                a3 = new ActionCharacterSet(c1_guid, c1, null);
+            Entry e1 = new Entry(10, new DateTime(1), "Somebody joined party", actions: new List<EntryAction>() { a1 }),
+                e2 = new Entry(20, new DateTime(2), "Someone Else joined party", actions: new List<EntryAction>() { a2 }),
+                e3 = new Entry(30, new DateTime(3), "Sombody left party", actions: new List<EntryAction>() { a3 });
+            CampaignDomain domain = new CampaignDomain() { entries = new List<Entry>() { e1, e2, e3 } };
+
+            e1.apply(domain.state);
+            e2.apply(domain.state);
+            e3.apply(domain.state);
+            domain.valid_entries = 3;
+
+            int idx_before = domain.get_timestamp_index(5);
+            Assert.AreEqual(idx_before, -1);
+            int idx0 = domain.get_timestamp_index(10);
+            Assert.AreEqual(idx0, 0);
+            int idx1 = domain.get_timestamp_index(25);
+            Assert.AreEqual(idx1, 1);
+            int idx2 = domain.get_timestamp_index(40);
+            Assert.AreEqual(idx2, 2);
+        }
+
+        [TestMethod]
+        public void test_get_timestamp_state() {
+            Character c1 = new Character("Somebody"), c2 = new Character("Someone Else");
+            Guid c1_guid = Guid.NewGuid(), c2_guid = Guid.NewGuid();
+            ActionCharacterSet a1 = new ActionCharacterSet(c1_guid, null, c1), a2 = new ActionCharacterSet(c2_guid, null, c2),
+                a3 = new ActionCharacterSet(c1_guid, c1, null);
+            Entry e1 = new Entry(10, new DateTime(1), "Somebody joined party", actions: new List<EntryAction>() { a1 }),
+                e2 = new Entry(20, new DateTime(2), "Someone Else joined party", actions: new List<EntryAction>() { a2 }),
+                e3 = new Entry(30, new DateTime(3), "Sombody left party", actions: new List<EntryAction>() { a3 });
+            CampaignDomain domain = new CampaignDomain() { entries = new List<Entry>() { e1, e2, e3 } };
+
+            e1.apply(domain.state);
+            e2.apply(domain.state);
+            e3.apply(domain.state);
+            domain.valid_entries = 3;
+
+            CampaignState state = domain.get_timestamp_state(15);
+            Assert.IsFalse(ReferenceEquals(state, domain.state));
+            Assert.AreEqual(state.characters.characters.Count, 1);
+            Assert.IsTrue(state.characters.characters.ContainsKey(c1_guid));
+            Assert.AreEqual(state.characters.active_characters.Count, 1);
+            Assert.IsTrue(state.characters.active_characters.Contains(c1_guid));
+        }
+
+        [TestMethod]
+        public void test_add_entry() {
+            Character c1 = new Character("Somebody"), c2 = new Character("Someone Else");
+            Guid c1_guid = Guid.NewGuid(), c2_guid = Guid.NewGuid();
+            ActionCharacterSet a1 = new ActionCharacterSet(c1_guid, null, c1), a2 = new ActionCharacterSet(c2_guid, null, c2),
+                a3 = new ActionCharacterSet(c1_guid, c1, null);
+            Entry e1 = new Entry(10, new DateTime(1), "Somebody joined party", actions: new List<EntryAction>() { a1 }),
+                e2 = new Entry(20, new DateTime(2), "Someone Else joined party", actions: new List<EntryAction>() { a2 }),
+                e3 = new Entry(30, new DateTime(3), "Sombody left party", actions: new List<EntryAction>() { a3 });
+            CampaignDomain domain = new CampaignDomain();
+
+            domain.add_entry(e1);
+            Assert.AreEqual(domain.state.characters.characters.Count, 1);
+            Assert.IsTrue(domain.state.characters.characters.ContainsKey(c1_guid));
+            Assert.AreEqual(domain.state.characters.active_characters.Count, 1);
+            Assert.IsTrue(domain.state.characters.active_characters.Contains(c1_guid));
+            Assert.AreEqual(domain.entries.Count, 1);
+            Assert.AreEqual(domain.entries[0], e1);
+            Assert.AreEqual(domain.valid_entries, 1);
+
+            domain.add_entry(e3);
+            Assert.AreEqual(domain.state.characters.characters.Count, 1);
+            Assert.IsTrue(domain.state.characters.characters.ContainsKey(c1_guid));
+            Assert.AreEqual(domain.state.characters.active_characters.Count, 0);
+            Assert.AreEqual(domain.entries.Count, 2);
+            Assert.AreEqual(domain.entries[0], e1);
+            Assert.AreEqual(domain.entries[1], e3);
+            Assert.AreEqual(domain.valid_entries, 2);
+
+            domain.add_entry(e2);
+            Assert.AreEqual(domain.state.characters.characters.Count, 2);
+            Assert.IsTrue(domain.state.characters.characters.ContainsKey(c1_guid));
+            Assert.IsTrue(domain.state.characters.characters.ContainsKey(c2_guid));
+            Assert.AreEqual(domain.state.characters.active_characters.Count, 1);
+            Assert.IsTrue(domain.state.characters.active_characters.Contains(c2_guid));
+            Assert.AreEqual(domain.entries.Count, 3);
+            Assert.AreEqual(domain.entries[0], e1);
+            Assert.AreEqual(domain.entries[1], e2);
+            Assert.AreEqual(domain.entries[2], e3);
+            Assert.AreEqual(domain.valid_entries, 3);
+        }
+
+        [TestMethod]
+        public void test_add_entry_invalid_states() {
+            Character c1 = new Character("Somebody"), c2 = new Character("Someone Else");
+            Guid c1_guid = Guid.NewGuid(), c2_guid = Guid.NewGuid();
+            ActionCharacterSet a1 = new ActionCharacterSet(c1_guid, null, c1), a2 = new ActionCharacterSet(c2_guid, null, c2),
+                a3 = new ActionCharacterSet(c1_guid, c1, null);
+            Entry e1 = new Entry(10, new DateTime(1), "Somebody joined party", actions: new List<EntryAction>() { a1 }),
+                e2 = new Entry(20, new DateTime(2), "Someone Else joined party", actions: new List<EntryAction>() { a2 }),
+                e3 = new Entry(30, new DateTime(3), "Sombody left party", actions: new List<EntryAction>() { a3 });
+            CampaignDomain domain = new CampaignDomain();
+
+            domain.add_entry(e3);
+            Assert.AreEqual(domain.state.characters.characters.Count, 0);
+            Assert.AreEqual(domain.state.characters.active_characters.Count, 0);
+            Assert.AreEqual(domain.entries.Count, 1);
+            Assert.AreEqual(domain.entries[0], e3);
+            Assert.AreEqual(domain.valid_entries, 0);
+
+            domain.add_entry(e2);
+            Assert.AreEqual(domain.state.characters.characters.Count, 1);
+            Assert.IsTrue(domain.state.characters.characters.ContainsKey(c2_guid));
+            Assert.AreEqual(domain.state.characters.active_characters.Count, 1);
+            Assert.IsTrue(domain.state.characters.active_characters.Contains(c2_guid));
+            Assert.AreEqual(domain.entries.Count, 2);
+            Assert.AreEqual(domain.entries[0], e2);
+            Assert.AreEqual(domain.entries[1], e3);
+            Assert.AreEqual(domain.valid_entries, 1);
+
+            domain.add_entry(e1);
+            Assert.AreEqual(domain.state.characters.characters.Count, 2);
+            Assert.IsTrue(domain.state.characters.characters.ContainsKey(c1_guid));
+            Assert.IsTrue(domain.state.characters.characters.ContainsKey(c2_guid));
+            Assert.AreEqual(domain.state.characters.active_characters.Count, 1);
+            Assert.IsTrue(domain.state.characters.active_characters.Contains(c2_guid));
+            Assert.AreEqual(domain.entries.Count, 3);
+            Assert.AreEqual(domain.entries[0], e1);
+            Assert.AreEqual(domain.entries[1], e2);
+            Assert.AreEqual(domain.entries[2], e3);
+            Assert.AreEqual(domain.valid_entries, 3);
+        }
+
+        [TestMethod]
+        public void test_remove_entry() {
+            Character c1 = new Character("Somebody"), c2 = new Character("Someone Else");
+            Guid c1_guid = Guid.NewGuid(), c2_guid = Guid.NewGuid();
+            ActionCharacterSet a1 = new ActionCharacterSet(c1_guid, null, c1), a2 = new ActionCharacterSet(c2_guid, null, c2),
+                a3 = new ActionCharacterSet(c1_guid, c1, null);
+            Entry e1 = new Entry(10, new DateTime(1), "Somebody joined party", actions: new List<EntryAction>() { a1 }),
+                e2 = new Entry(20, new DateTime(2), "Someone Else joined party", actions: new List<EntryAction>() { a2 }),
+                e3 = new Entry(30, new DateTime(3), "Sombody left party", actions: new List<EntryAction>() { a3 });
+            CampaignDomain domain = new CampaignDomain() { entries = new List<Entry>() { e1, e2, e3 } };
+
+            e1.apply(domain.state);
+            e2.apply(domain.state);
+            e3.apply(domain.state);
+            domain.valid_entries = 3;
+
+            domain.remove_entry(1);
+            Assert.AreEqual(domain.state.characters.characters.Count, 1);
+            Assert.IsTrue(domain.state.characters.characters.ContainsKey(c1_guid));
+            Assert.AreEqual(domain.state.characters.active_characters.Count, 0);
+            Assert.AreEqual(domain.entries.Count, 2);
+            Assert.AreEqual(domain.entries[0], e1);
+            Assert.AreEqual(domain.entries[1], e3);
+            Assert.AreEqual(domain.valid_entries, 2);
+
+            domain.remove_entry(0);
+            Assert.AreEqual(domain.state.characters.characters.Count, 0);
+            Assert.AreEqual(domain.state.characters.active_characters.Count, 0);
+            Assert.AreEqual(domain.entries.Count, 1);
+            Assert.AreEqual(domain.entries[0], e3);
+            Assert.AreEqual(domain.valid_entries, 0);
+        }
+
+        [TestMethod]
+        public void test_remove_entry_invalidate() {
+            Character c1 = new Character("Somebody"), c2 = new Character("Someone Else");
+            Guid c1_guid = Guid.NewGuid(), c2_guid = Guid.NewGuid();
+            ActionCharacterSet a1 = new ActionCharacterSet(c1_guid, null, c1), a2 = new ActionCharacterSet(c2_guid, null, c2),
+                a3 = new ActionCharacterSet(c1_guid, c1, null);
+            Entry e1 = new Entry(10, new DateTime(1), "Somebody joined party", actions: new List<EntryAction>() { a1 }),
+                e2 = new Entry(20, new DateTime(2), "Someone Else joined party", actions: new List<EntryAction>() { a2 }),
+                e3 = new Entry(30, new DateTime(3), "Sombody left party", actions: new List<EntryAction>() { a3 });
+            CampaignDomain domain = new CampaignDomain() { entries = new List<Entry>() { e1, e2, e3 } };
+
+            e1.apply(domain.state);
+            e2.apply(domain.state);
+            e3.apply(domain.state);
+            domain.valid_entries = 3;
+
+            domain.remove_entry(1, true);
+            Assert.AreEqual(domain.state.characters.characters.Count, 1);
+            Assert.IsTrue(domain.state.characters.characters.ContainsKey(c1_guid));
+            Assert.AreEqual(domain.state.characters.active_characters.Count, 1);
+            Assert.IsTrue(domain.state.characters.active_characters.Contains(c1_guid));
+            Assert.AreEqual(domain.entries.Count, 2);
+            Assert.AreEqual(domain.entries[0], e1);
+            Assert.AreEqual(domain.entries[1], e3);
+            Assert.AreEqual(domain.valid_entries, 1);
         }
     }
 }
