7714cb5889663e30c3d5d0f4438d3252dbeb0865
manveti <manveti@users.noreply.github.com>
2021-04-01 21:57:27 -0700

    Initial work on campaign save state.

---
+++ CampLog/CampaignSave.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace CampLog {
+    [Serializable]
+    public class CampaignSave {
+        public CampaignDomain domain;
+        public Calendar calendar;
+        //TODO: public CharacterSheet character_sheet;
+        public bool show_past_events;
+        public bool show_inactive_tasks;
+
+        public CampaignSave(Calendar calendar) {
+            this.domain = new CampaignDomain();
+            this.calendar = calendar;
+            //TODO: character_sheet
+            this.show_past_events = false;
+            this.show_inactive_tasks = false;
+        }
+    }
+}
---
+++ CampLogTest/TestCampaignSave.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+using CampLog;
+
+namespace CampLogTest {
+    [TestClass]
+    public class TestCampaignSave {
+        [TestMethod]
+        public void test_serialization() {
+            Character chr = new Character("Somebody");
+            CampaignSave foo = new CampaignSave(new Calendar()), bar;
+
+            Guid chr_guid = foo.domain.state.characters.add_character(chr);
+
+            DataContractSerializer fmt = new DataContractSerializer(typeof(CampaignSave));
+            using (System.IO.MemoryStream ms = new System.IO.MemoryStream()) {
+                fmt.WriteObject(ms, foo);
+                ms.Seek(0, System.IO.SeekOrigin.Begin);
+                System.Xml.XmlDictionaryReader xr = System.Xml.XmlDictionaryReader.CreateTextReader(ms, new System.Xml.XmlDictionaryReaderQuotas());
+                bar = (CampaignSave)(fmt.ReadObject(xr, true));
+            }
+            Assert.AreEqual(foo.domain.state.characters.characters.Count, bar.domain.state.characters.characters.Count);
+            Assert.IsTrue(bar.domain.state.characters.characters.ContainsKey(chr_guid));
+            Assert.AreEqual(bar.domain.state.characters.characters[chr_guid].name, "Somebody");
+            Assert.AreEqual(foo.calendar.GetType(), bar.calendar.GetType());
+            //TODO: character_sheet
+            Assert.AreEqual(foo.show_past_events, bar.show_past_events);
+            Assert.AreEqual(foo.show_inactive_tasks, bar.show_inactive_tasks);
+        }
+    }
+}
