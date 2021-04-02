1178936b6a708599943fa202b5cd1974467a40ad
manveti <manveti@users.noreply.github.com>
2021-04-01 21:57:50 -0700

    Initial work on campaign creation.

---
+++ CampLog/CampaignPropsWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+
+namespace CampLog {
+    public partial class CampaignPropsWindow : Window {
+        public bool valid;
+        private CalendarParameters parameters;
+
+        public CampaignPropsWindow() {
+            this.valid = false;
+            this.parameters = null;
+            InitializeComponent();
+            this.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            this.SizeToContent = SizeToContent.WidthAndHeight;
+            this.calendar_box.ItemsSource = CalendarSpecs.specs.Keys;
+            this.calendar_box.SelectedValue = "None";
+            //TODO: populate this.charsheet_box
+        }
+
+        private void do_ok(object sender, RoutedEventArgs e) {
+            this.valid = true;
+            this.Close();
+        }
+
+        private void do_cancel(object sender, RoutedEventArgs e) {
+            this.Close();
+        }
+
+        public Calendar get_calendar() {
+            string calendar_name = this.calendar_box.SelectedValue as string;
+            if ((calendar_name is null) || (!CalendarSpecs.specs.ContainsKey(calendar_name))) { throw new InvalidOperationException(); }
+            return CalendarSpecs.specs[calendar_name].get_calendar(this.parameters);
+        }
+    }
+}
--- CampLog/MainWindow.xaml.cs
+++ CampLog/MainWindow.xaml.cs
@@ -13,16 +13,60 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace CampLog
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+namespace CampLog {
+    public partial class MainWindow : Window {
+        private CampaignSave state;
+        private bool state_dirty;
+        private string save_path;
+
+        public MainWindow() {
+            this.save_path = null;
+            this.state = null;
+            this.state_dirty = false;
             InitializeComponent();
+        }
+
+        private void new_campaign(object sender, RoutedEventArgs e) {
+            if (this.state_dirty) {
+                string msg = "Do you want to save changes to " + (this.save_path ?? "Untitled") + "?";
+                MessageBoxResult result = MessageBox.Show(msg, "Campaign Log", MessageBoxButton.YesNoCancel);
+                if (result == MessageBoxResult.Cancel) { return; }
+                if (result != MessageBoxResult.No) {
+                    //TODO: save this.state
+                    this.state_dirty = false;
+                }
+            }
+
+            CampaignPropsWindow props_window = new CampaignPropsWindow() { Owner = this };
+            props_window.ShowDialog();
+            if (!props_window.valid) { return; }
+
+            Calendar cal;
+            try {
+                cal = props_window.get_calendar();
+                //TODO: character sheet
+            }
+            catch (InvalidOperationException) { return; }
+
+            this.state = new CampaignSave(cal);
+            this.state_dirty = false;
+            this.save_path = null;
+
+            this.save_opt.IsEnabled = true;
+            this.save_as_opt.IsEnabled = true;
+            this.calendar_cfg_opt.IsEnabled = true;
+            this.charsheet_cfg_opt.IsEnabled = true;
+            this.item_library_opt.IsEnabled = true;
+            this.revert_opt.IsEnabled = true;
+            //TODO: characters_list, char_add_but, char_rem_but, char_view_but
+            //TODO: inventories_list, inv_add_but, inv_rem_but, inv_view_but
+            //TODO: session_num_box, current_timestamp_box
+            //TODO: entries_list, ent_add_but, ent_rem_but, ent_view_but
+            this.show_past_events_box.IsChecked = this.state.show_past_events;
+            //TODO: events_list, event_add_but, event_rem_but, event_view_but
+            this.show_inactive_tasks_box.IsChecked = this.state.show_inactive_tasks;
+            //TODO: tasks_list, task_add_but, task_rem_but, task_resolve_but, task_view_but
+            //TODO: topics_list, topic_add_but, topic_rem_but, topic_view_but
         }
     }
 }
