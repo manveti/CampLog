f4b1e12d81c8c8b7b87d92e2ce05d4d348a22411
manveti <manveti@users.noreply.github.com>
2021-06-08 00:23:01 -0700

    Suppress the value changed event when updating day box if we're also going to update time box so we don't send events when we're in an inconsistent state.

--- CampLog/CampaignDateCalendarControl.xaml.cs
+++ CampLog/CampaignDateCalendarControl.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows.Controls;
 namespace CampLog {
     public partial class CampaignDateCalendarControl : UserControl, ICalendarControl {
         private Action _value_changed;
+        private bool suppress_change_event = false;
 
         public CampaignDateCalendarControl() {
             InitializeComponent();
@@ -30,12 +31,15 @@ namespace CampLog {
         public decimal calendar_value {
             get => (decimal)((this.day_box.Value * CampaignDateCalendar.DAY_LENGTH) + this.time_box.Value);
             set {
+                this.suppress_change_event = true;
                 this.day_box.Value = (long)(value / CampaignDateCalendar.DAY_LENGTH);
+                this.suppress_change_event = false;
                 this.time_box.Value = (double)(value % CampaignDateCalendar.DAY_LENGTH);
             }
         }
 
         private void handleValueChange(object sender, TextChangedEventArgs e) {
+            if (this.suppress_change_event) { return; }
             if (this._value_changed is not null) {
                 this._value_changed();
             }
